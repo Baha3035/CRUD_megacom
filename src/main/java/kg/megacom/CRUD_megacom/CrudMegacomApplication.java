ceed8a76c1de432d360f3c79e842ecd39d44cb08
Baha <batmaaanpls@gmail.com>
2021-07-12 18:30:20 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/CRUD_megacom/CrudMegacomApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.CRUD_megacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMegacomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMegacomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megacom/CRUD_megacom/CrudMegacomApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.CRUD_megacom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMegacomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
